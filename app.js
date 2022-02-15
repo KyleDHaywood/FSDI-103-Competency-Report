38ce628b34b3eeefbb36a6eafe762707a1c1e76a
Kyle <kylehaywood@hotmail.com>
2022-02-14 22:49:58 -0500

    Good working copy

--- app.js
+++ app.js
@@ -5,33 +5,33 @@ let energy = 50;
 let feed = () => {
   console.log("Feed Function");
   if (hunger > 0 && hunger < 101) {
-    hunger = hunger + ((100 - hunger) / 1.5);
+    hunger = hunger + (100 - hunger) / 1.5;
   }
   if (happiness < 101) {
-    happiness = happiness + ((100 - happiness) * 0.75) ;
+    happiness = happiness + (100 - happiness) * 0.75;
   }
   if (energy < 85) {
-    energy = energy + (energy * 0.2)
-  };
+    energy = energy + energy * 0.2;
+  }
   displayInfo();
   setGauges();
 };
 function pet() {
   if (happiness < 100) {
-    happiness = happiness + ((100 - happiness) * 0.05);
+    happiness = happiness + (100 - happiness) * 0.05;
   }
   displayInfo();
   setGauges();
 }
 function play() {
   if (happiness < 101) {
-    happiness = happiness + ((100 - happiness)/3);
+    happiness = happiness + (100 - happiness) / 3;
   }
   if (hunger > 25 && hunger < 101) {
-    hunger = hunger - ((100 - hunger) * 0.5);
+    hunger = hunger - (100 - hunger) * 0.5;
   }
   if (energy > 0) {
-    energy = energy - (energy * 0.4);
+    energy = energy - energy * 0.4;
   }
   displayInfo();
   setGauges();
@@ -47,22 +47,19 @@ function displayInfo() {
 let gauge = document.getElementsByClassName(".gauge");
 let gaugeElements = document.querySelectorAll(".gauge");
 
-
 function setGaugeValue(gauge, points) {
- // console.log(points);
-   var bar= document.getElementById(gauge);
-   console.log(gauge);
-    bar.style.transform = `rotate(${points*1.8}deg)`;
-    bar.style.transition = `2s ease-out`;
+  // console.log(points);
+  var bar = document.getElementById(gauge);
+  console.log(gauge);
+  bar.style.transform = `rotate(${points * 1.8}deg)`;
+  bar.style.transition = `2s ease-out`;
+}
 
-  };
-
-function setGauges(){
-//  console.log(happiness);
- setGaugeValue("happinessGauge", happiness);
- setGaugeValue("energyGauge", energy);
- setGaugeValue("hungerGauge", hunger);
-
+function setGauges() {
+  //  console.log(happiness);
+  setGaugeValue("happinessGauge", happiness);
+  setGaugeValue("energyGauge", energy);
+  setGaugeValue("hungerGauge", hunger);
 }
 /*
   gaugeElements.forEach((gauge, value) => {
